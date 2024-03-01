dc7ad0fa1886872ad7acfccb65d25f4231fe5fd2
abrs7 <abrishsambong@gmail.com>
2024-03-01 22:41:23 +0300

    update models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length = 2045)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    
